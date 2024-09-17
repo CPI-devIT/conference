f936b17eac8a11d378aa0554ade089376279b823
Anna-Anka <anjbekrenewa@gmail.com>
2024-09-17 11:12:13 +0300

    first commit

---
+++ src/js/templates/_burger.js
@@ -0,0 +1,45 @@
+import { disableScroll, enableScroll } from '../utils/index.js';
+
+export const burger = () => {
+    const burgerButton = document.querySelector('[data-burger-button]');
+    const menu = document.querySelector('[data-menu]');
+    const menuLinks = document.querySelectorAll('[data-menu-link]');
+    const overlay = document.querySelector('[data-menu-overlay]');
+
+    const checkClass = () => {
+        if (burgerButton.classList.contains('burger-button--active')) {
+            burgerButton.setAttribute('aria-expanded', 'true');
+            burgerButton.setAttribute('aria-label', 'закрыть меню');
+            disableScroll();
+        } else {
+            burgerButton.setAttribute('aria-expanded', 'false');
+            burgerButton.setAttribute('aria-label', 'открыть меню');
+            enableScroll();
+        }
+    };
+
+    const hideBurger = () => {
+        burgerButton.classList.remove('burger-button--active');
+        menu.classList.remove('burger-menu--active');
+        overlay.classList.remove('overlay--active');
+    }
+
+    burgerButton.addEventListener('click', () => {
+        burgerButton.classList.toggle('burger-button--active');
+        menu.classList.toggle('burger-menu--active');
+        overlay.classList.toggle('overlay--active');
+        checkClass();
+    });
+
+    overlay.addEventListener('click', () => {
+        hideBurger();
+        checkClass();
+    });
+
+    menuLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            hideBurger();
+            enableScroll();
+        });
+    });
+};
---
+++ src/js/utils/index.js
@@ -0,0 +1,10 @@
+import { disableScroll } from './_disable-scroll.js';
+import { enableScroll } from './_enable-scroll.js';
+import { throttle } from './_throttle.js';
+// import { fixFullheight } from './_fix-fullheight.js';
+// import { getHeaderHeight } from './_get-header-height.js';
+// import { mobileCheck } from './_mobile-check.js';
+
+// fixFullheight();
+// getHeaderHeight();
+// mobileCheck();
