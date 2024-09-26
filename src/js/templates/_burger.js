4a734f9b169428a5898f4d6743718947fef1d1ff
Anna-Anka <anjbekrenewa@gmail.com>
2024-09-26 14:16:31 +0300

    refactor header

--- src/js/templates/_burger.js
+++ src/js/templates/_burger.js
@@ -7,30 +7,18 @@ export const burger = () => {
     const overlay = document.querySelector('[data-menu-overlay]');
     const buttonsContainer = document.querySelector(".header__account");
     const partner = document.querySelector(".header__partners");
-    const block = document.querySelector(".header");
+    const header = document.querySelector(".header");
 
 
     const checkClass = () => {
         if (burgerButton.classList.contains('burger-button--active')) {
             burgerButton.setAttribute('aria-expanded', 'true');
             burgerButton.setAttribute('aria-label', 'закрыть меню');
-            // disableScroll();
+            disableScroll();
         } else {
             burgerButton.setAttribute('aria-expanded', 'false');
             burgerButton.setAttribute('aria-label', 'открыть меню');
-            // enableScroll();
-        }
-    };
-
-       const showButton = () => {
-        if (burgerButton.classList.contains('burger-button--active')) {
-            partner.classList.add('header__partners--hidden');
-            buttonsContainer.classList.add('header__account--active');
-            block.classList.add('header--active');
-        } else {
-            partner.classList.remove('.header__partners--hidden');
-            buttonsContainer.classList.remove('header__account--active');
-            block.classList.remove('header--active');
+            enableScroll();
         }
     };
 
@@ -38,29 +26,31 @@ export const burger = () => {
         burgerButton.classList.remove('burger-button--active');
         menu.classList.remove('burger-menu--active');
         overlay.classList.remove('overlay--active');
+        partner.classList.remove('header__partners--hidden');
+        buttonsContainer.classList.remove('header__account--active');
+        header.classList.remove('header--active');
     }
 
     burgerButton.addEventListener('click', () => {
         burgerButton.classList.toggle('burger-button--active');
         menu.classList.toggle('burger-menu--active');
-        // overlay.classList.toggle('overlay--active');
+        overlay.classList.toggle('overlay--active');
+        partner.classList.toggle('header__partners--hidden');
+        buttonsContainer.classList.toggle('header__account--active');
+        header.classList.toggle('header--active');
         checkClass();
-        showButton();
     });
 
-    // overlay.addEventListener('click', () => {
-    //     hideBurger();
-    //     checkClass();
-    //     showButton();
-    // });
+    overlay.addEventListener('click', () => {
+        hideBurger();
+        checkClass();
+    });
 
     menuLinks.forEach((link) => {
         link.addEventListener('click', () => {
             hideBurger();
+            checkClass();
             enableScroll();
         });
     });
-
-
-
 };
--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -1,6 +1,6 @@
-import { disableScroll } from './_disable-scroll.js';
-import { enableScroll } from './_enable-scroll.js';
-import { throttle } from './_throttle.js';
+export { disableScroll } from './_disable-scroll.js';
+export { enableScroll } from './_enable-scroll.js';
+export { throttle } from './_throttle.js';
 // import { fixFullheight } from './_fix-fullheight.js';
 // import { getHeaderHeight } from './_get-header-height.js';
 // import { mobileCheck } from './_mobile-check.js';
