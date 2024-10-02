b2a9e5ea36d18b51ec3624a09ea77032f6a171dd
Aleksandr Ovsyannikov <116282471+AlexOneWRLD@users.noreply.github.com>
2024-10-02 16:36:06 +0300

    fix stream

--- src/js/project/index.js
+++ src/js/project/index.js
@@ -1,10 +1,7 @@
 import './sliders/_news-slider.js';
 import './_inter-map.js';
-<<<<<<< HEAD
 import {stream} from './_stream.js'
 stream();
-=======
 import { animateHeroImage } from './_hero-animation.js'
 
 animateHeroImage();
->>>>>>> origin
