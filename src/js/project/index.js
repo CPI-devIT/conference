d4149758f853ac46cd886eb53cceedf34d598f25
Anna-Anka <anjbekrenewa@gmail.com>
2024-10-03 12:08:00 +0300

    refactor map

--- src/js/project/index.js
+++ src/js/project/index.js
@@ -1,5 +1,6 @@
 import './sliders/_news-slider.js';
-import './_inter-map.js';
+// import './map/_inter-map.js';
+import './map/_test-map.js';
 import {stream} from './_stream.js'
 stream();
 import { animateHeroImage } from './_hero-animation.js'
