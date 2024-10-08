b06aadef66aabc3a7d1de8f0a09f8a996a545af7
Anna-Anka <anjbekrenewa@gmail.com>
2024-10-08 23:03:24 +0300

    refactor header-sticky

--- src/js/templates/_header-sticky.js
+++ src/js/templates/_header-sticky.js
@@ -2,17 +2,15 @@ import { throttle } from '../utils/index.js';
 
 export const stickyHeader = () => {
     const header = document.querySelector('.header');
-    const hero = document.querySelector('.hero');
     const burgerMenu = document.querySelector('.burger-menu');
 
-    if (hero && header) {
-        const heroHeight = hero.offsetHeight;
+    if (header) {
 
         const changeClasses = () => {
             if (!burgerMenu.classList.contains('burger-menu--active')) {
                 const scrollDistance = window.scrollY;
 
-                if (scrollDistance > heroHeight) {
+                if (scrollDistance > 500) {
                     header.classList.add('header--fixed');
                 } else {
                     header.classList.remove('header--fixed');
