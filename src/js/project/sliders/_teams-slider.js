7d3a21c5264d8495484d7a3eade0fd2fed97450d
Aleksandr Ovsyannikov <116282471+AlexOneWRLD@users.noreply.github.com>
2024-10-17 17:09:38 +0300

    add org slider

--- src/js/project/sliders/_teams-slider.js
+++ src/js/project/sliders/_teams-slider.js
@@ -18,3 +18,19 @@ if (document.querySelector('.team__swiper')) {
 
     });
 }
+
+if (document.querySelector('.team-org__swiper')) {
+    new Swiper('.team-org__swiper', {
+
+        speed: 800,
+        slidesPerGroup: 1,
+        slidesPerView: 1,
+        spaceBetween: 10,
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+    });
+}
